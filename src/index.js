a4c701112930c075a9a10104cf1a766ae9272b4a
Alina-Pishak <alinapishak@icloud.com>
2023-07-30 11:06:04 +0300

    fix empty arr

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ fetchBreeds()
         data.map(({ id, name }) => breedList.insertAdjacentHTML('beforeend', createOptions(id, name)));
         loaderEl.classList.add('visually-hidden');
         new SlimSelect({
-          select: '#single',
+            select: '#breeds',
         })
         breedList.classList.remove('visually-hidden');
         breedList.addEventListener('change', getInfo);
@@ -30,6 +30,11 @@ function getInfo(evt) {
     loaderEl.classList.remove('visually-hidden');
     fetchCatByBreed(evt.currentTarget.value)
         .then(({ data }) => {
+            if (data.length < 1) {
+                Report.failure("Error", "We can't find any information about this breed:( Try find another breed.", "Ok");
+                loaderEl.classList.add('visually-hidden');
+                return;
+            }
             data.map(({ url, breeds }) => catInfo.innerHTML = createMarkup(url, breeds));
             catInfo.classList.remove('visually-hidden');
             loaderEl.classList.add('visually-hidden');
